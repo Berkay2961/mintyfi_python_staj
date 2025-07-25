bc6f753c3b159098cf050f8f2538828ff6f9ccb8
Berkay2961 <156474257+Berkay2961@users.noreply.github.com>
2025-07-25 15:07:59 +0300

    python giris

---
+++ day-1 staj.py
@@ -0,0 +1,9 @@
+#ekrana yazı yazdırma
+print("Merhaba Python")
+print("Ad:Berkay")
+print("Soyad:Sahin")
+print("Yas:20")
+print("Meslek:Ogrenci")
+#ekrana yazdirma diger yol
+print("Ad:Berkay,Soyad:Sahin,Yas:20,Meslek:Ogrenci")
+print("elma","armut","seftali","kiraz",sep="\n",end="...")
